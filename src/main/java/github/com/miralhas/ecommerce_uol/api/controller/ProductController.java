3e11c40850dadda3bb2101e85c1714b74e14620a
Miralhas <victor2.miralhas@gmail.com>
2024-07-03 11:47:14 -0300

    feat: product controller & service classes

--- src/main/java/github/com/miralhas/ecommerce_uol/api/controller/ProductController.java
+++ src/main/java/github/com/miralhas/ecommerce_uol/api/controller/ProductController.java
@@ -1,4 +1,64 @@
 package github.com.miralhas.ecommerce_uol.api.controller;
 
+import github.com.miralhas.ecommerce_uol.api.dto.ProductDTO;
+import github.com.miralhas.ecommerce_uol.api.dto.ProductSummaryDTO;
+import github.com.miralhas.ecommerce_uol.api.dto.input.ProductInput;
+import github.com.miralhas.ecommerce_uol.api.dto_mapper.ProductMapper;
+import github.com.miralhas.ecommerce_uol.api.dto_mapper.ProductUnmapper;
+import github.com.miralhas.ecommerce_uol.domain.model.Product;
+import github.com.miralhas.ecommerce_uol.domain.service.ProductService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/products")
 public class ProductController {
+
+    private final ProductMapper productMapper;
+    private final ProductUnmapper productUnmapper;
+    private final ProductService productService;
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<ProductSummaryDTO> getAllProducts() {
+        List<Product> products = productService.getAll();
+        return productMapper.toSummaryCollectionModel(products);
+    }
+
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public ProductDTO getProductById(@PathVariable Long id) {
+        Product product = productService.findByIdOrException(id);
+        return productMapper.toModel(product);
+    }
+
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ProductDTO createProduct(@RequestBody @Valid ProductInput productInput) {
+        Product product = productUnmapper.toDomainObject(productInput);
+        product = productService.save(product);
+        return productMapper.toModel(product);
+    }
+
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public ProductDTO updateProduct(@PathVariable Long id, @RequestBody @Valid ProductInput productInput) {
+        Product product = productService.update(id, productInput);
+        return productMapper.toModel(product);
+    }
+
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteProduct(@PathVariable Long id) {
+        productService.delete(id);
+    }
 }
--- src/main/java/github/com/miralhas/ecommerce_uol/domain/service/ProductService.java
+++ src/main/java/github/com/miralhas/ecommerce_uol/domain/service/ProductService.java
@@ -1,4 +1,53 @@
 package github.com.miralhas.ecommerce_uol.domain.service;
 
+import github.com.miralhas.ecommerce_uol.api.dto.input.ProductInput;
+import github.com.miralhas.ecommerce_uol.api.dto_mapper.ProductUnmapper;
+import github.com.miralhas.ecommerce_uol.domain.exception.ProductNotFoundException;
+import github.com.miralhas.ecommerce_uol.domain.model.Product;
+import github.com.miralhas.ecommerce_uol.domain.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class ProductService {
+
+    private final ProductRepository productRepository;
+    private final ProductUnmapper productUnmapper;
+
+    public List<Product> getAll() {
+        return productRepository.findAll();
+    }
+
+
+    public Product findByIdOrException(Long id) {
+        return productRepository.findById(id)
+                .orElseThrow(() -> new ProductNotFoundException(id));
+    }
+
+
+    @Transactional
+    public Product save(Product product) {
+        return productRepository.save(product);
+    }
+
+
+    @Transactional
+    public Product update(Long id, ProductInput productInput) {
+        Product product = findByIdOrException(id);
+        productUnmapper.copyToDomainObject(productInput, product);
+        return productRepository.save(product);
+    }
+
+
+    @Transactional
+    public void delete(Long id) {
+        productRepository.deleteById(id);
+    }
+
+
 }
