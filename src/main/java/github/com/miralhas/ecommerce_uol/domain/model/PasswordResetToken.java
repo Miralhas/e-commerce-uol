259f2f6173bca510c067d50d6023a825785097f3
Miralhas <victor2.miralhas@gmail.com>
2024-07-15 18:14:48 -0300

    feat: ResetPasswordToken Model and flyway migration

---
+++ src/main/java/github/com/miralhas/ecommerce_uol/domain/model/PasswordResetToken.java
@@ -0,0 +1,40 @@
+package github.com.miralhas.ecommerce_uol.domain.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.proxy.HibernateProxy;
+
+import java.util.Objects;
+
+@Getter
+@Setter
+@Entity
+public class PasswordResetToken {
+
+    @Id
+    private Long id;
+
+    @Column(nullable = false)
+    private String token;
+
+    @MapsId
+    @OneToOne(targetEntity = User.class, fetch = FetchType.LAZY)
+    private User user;
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        PasswordResetToken that = (PasswordResetToken) o;
+        return getId() != null && Objects.equals(getId(), that.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+    }
+}
